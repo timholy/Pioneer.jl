fbe794a7e33eb3ea386709f73a2d7fd30c3b4918
Documenter.jl <documenter@juliadocs.github.io>
2024-08-01 23:32:49 +0000

    build based on 0a6a5ea

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Pioneer","category":"page"},{"location":"#Pioneer","page":"Home","title":"Pioneer","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Pioneer.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Pioneer]","category":"page"}]
+}
